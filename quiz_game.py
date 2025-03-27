318d7cb1597491f3143eacbcedc3c1b7ff62159d
LisaKajula <kisakl4414045_aehit@students.vizja.pl>
2025-03-27 21:49:15 +0100

    first try

---
+++ quiz_game.py
@@ -0,0 +1,10 @@
+print("welcome to my game " )
+
+player=input("do you want to play? ")
+if playing !="yes" :
+    quit()
+    print ("lets play ")
+    answer=input("what is CPU in full ? ")
+    if answer ==="central processing unit":
+        print("that's correct")
+
